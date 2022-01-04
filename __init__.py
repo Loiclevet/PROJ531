fe4533aa56e228c100a55a1739f4329d2d0640ec
RFaska <93133836+playerRC@users.noreply.github.com>
2022-01-04 16:11:08 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 #on importe le module chess pour avoir accès aux fonctionnalités de base du jeu d'échecs
 import chess
 
-#représentation de l'échiquier
+#-----------représentation de l'échiquier
 board=chess.Board()
 print(board)
 
-#génération des coups légaux
-def polyglot(board):
-  #trouve toutes les entrées pour la position de départ
+#-----------génération de toutes les ouvertures possibles
+def polyOuverture(board):
+  ouverture=[]
+  #trouve toutes les ouvertures possibles pour la position de départ
   with chess.polyglot.open_reader("data/polyglot/performance.bin") as reader:
     for entry in reader.find_all(board):
-      print(entry.move, entry.weight, entry.learn)
+      ouverture.append((entry.move, entry.weight, entry.learn))
+  return ouverture
