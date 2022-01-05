b33ed529016594204b21250714adad282253d4b3
RFaska <93133836+playerRC@users.noreply.github.com>
2022-01-05 15:05:41 +0100

    Update Jouer.py

--- src/Jouer.py
+++ src/Jouer.py
@@ -3,6 +3,17 @@ class Partie:
   
   def __init__(self, board):
     self.board=board
+   
+  def get_move(self, legal_moves):
+    
+        position_initiale = input("Quelle pièce voulez-vous déplacer? ")
+        position_finale = input("Jusqu'à où BG? ")
+        while chess.Move.from_uci(pos_init + pos_fin) not in legal_moves:
+            print("Non pas comme ça BG")
+            position_initiale = input("Quelle pièce voulez-vous déplacer? ")
+            position_finale = input("usqu'à où BG? ")
+
+        return position_initiale + position_finale
     
   def start(self):
 
