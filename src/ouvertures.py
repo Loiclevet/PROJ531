f8075d2d63b54d780e82ef7d05c9794905f5e722
RFaska <93133836+playerRC@users.noreply.github.com>
2022-01-05 14:42:14 +0100

    Update ouvertures.py

--- src/ouvertures.py
+++ src/ouvertures.py
@@ -1,1 +1,10 @@
+import chess.polyglot
 
+#-----------génération de toutes les ouvertures possibles
+def polyOuverture(board):
+  ouverture=[]
+  #trouve toutes les ouvertures possibles pour la position de départ
+  with chess.polyglot.open_reader("data/polyglot/performance.bin") as reader:
+    for entry in reader.find_all(board):
+      ouverture.append((entry.move, entry.weight, entry.learn))
+  return ouverture
