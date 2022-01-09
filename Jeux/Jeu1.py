63b484797a95cd5d30699f5994a45c4982419353
RFaska <93133836+playerRC@users.noreply.github.com>
2022-01-09 14:45:27 +0100

    Joueur VS Joueur

--- Jeux/Jeu1.py
+++ Jeux/Jeu1.py
@@ -5,5 +5,15 @@ import Fonctions.py as fct
 
 board=chess.Board()
 
+def bouger_piece():
+  position_initiale = input("Quelle pièce voulez-vous déplacer? ")
+  position_finale = input("Jusqu'à où BG? ")
+  while chess.Move.from_uci(position_initiale + position_finale) not in board.legal_moves:
+    print("Non pas comme ça BG")
+    position_initiale = input("Quelle pièce voulez-vous déplacer? ")
+    position_finale = input("Jusqu'à où BG? ")
+  move=position_initiale+position_finale
+  board.push_uci(move)
+
 while not(board.is_game_over()):
   pass
