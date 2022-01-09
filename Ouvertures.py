e3d15fc130d2e5263371ee615e8e5dcfb234e330
RFaska <93133836+playerRC@users.noreply.github.com>
2022-01-09 14:59:16 +0100

    Propose une bibliothèque d'ouvertures

--- Ouvertures.py
+++ Ouvertures.py
@@ -1,1 +1,14 @@
+import chess
+import chess.polyglot
 
+'''
+Cette fonction est utile pour la génération de toutes les ouvertures possibles
+'''
+
+def polyOuverture(board):
+  ouverture=[]
+  #trouve toutes les ouvertures possibles pour la position de départ
+  with chess.polyglot.open_reader("data/polyglot/performance.bin") as reader:
+    for entry in reader.find_all(board):
+      ouverture.append((entry.move, entry.weight, entry.learn))
+  return ouverture
