03fecca03ee169c9ba140b75599e79a7e6aa25ad
RFaska <93133836+playerRC@users.noreply.github.com>
2022-01-09 21:10:56 +0100

    Liste des ouvertures possibles

--- Ouvertures.py
+++ Ouvertures.py
@@ -8,7 +8,7 @@ Cette fonction est utile pour la génération de toutes les ouvertures possibles
 def polyOuverture(board):
   ouverture=[]
   #trouve toutes les ouvertures possibles pour la position de départ
-  with chess.polyglot.open_reader("data/polyglot/performance.bin") as reader:
+  with chess.polyglot.open_reader("bookOuvertures.bin") as reader:
     for entry in reader.find_all(board):
       ouverture.append((entry.move, entry.weight, entry.learn))
   return ouverture
